e197dcd06f219e5b3f3fc31f06c28706cf544226
Luis Daniel Gonzalez <gonzalez2dani3@gmail.com>
2024-07-03 15:18:22 -0600

    Creacion de proyecto

--- Abarrotes/Abarrotes/Program.cs
+++ Abarrotes/Abarrotes/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Mantenedor}/{action=Login}/{id?}");
 
 app.Run();
